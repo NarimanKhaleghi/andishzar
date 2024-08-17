6c13adc4a00a0b6a56242323496731aee1e29f63
Nariman Khaleghi <54586611+NarimanKhaleghi@users.noreply.github.com>
2024-08-17 20:51:12 +0330

    quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "fa-IR",
-    baseUrl: "narimankhaleghi.github.io/andishzar",
+    baseUrl: "andishzar.ir",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
